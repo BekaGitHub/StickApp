c57216ebdafa43db3cd3807d12c6068af7c54f97
EmapT Projekt <EmapT Projekt>
2017-01-06 05:41:53 +0100

    UDP CommandSender

--- app/src/main/java/app/dfki/de/stickapp/connect/TcpClient.java
+++ app/src/main/java/app/dfki/de/stickapp/connect/TcpClient.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -18,48 +20,20 @@ public class TcpClient {
     //hiz guest and saarland 134.96.79.69
     private static final String SERVER_IP = "192.168.2.101"; //your computer IP address
     private static final int SERVER_PORT = 64000;
-
-
-    // used to send messages
-    private PrintWriter mBufferOut;
-
-    /**
-     * Sends the message entered by client to the server
-     *
-     * @param message text entered by client
-     */
-    private void sendMessage(String message) {
-        if (mBufferOut != null && !mBufferOut.checkError()) {
-            mBufferOut.println(message);
-            mBufferOut.flush();
-        }
-    }
-
-    /**
-     * Close the connection and release the members
-     */
-    public void stopClient() {
-        Log.i("Debug", "stopClient");
-
-        if (mBufferOut != null) {
-            mBufferOut.flush();
-            mBufferOut.close();
-        }
-        mBufferOut = null;
-    }
-
+    byte[] sendData;
+    
     public void run(String message){
         try {
+            //create a socket to make the connection with the server
+            DatagramSocket senderSocket = new DatagramSocket(SERVER_PORT);
             //here you must put your computer's IP address.
             InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
 
-            //create a socket to make the connection with the server
-            Socket socket = new Socket(serverAddr, SERVER_PORT);
+            sendData = message.getBytes();
 
-            //sends the message to the server
-            mBufferOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-            sendMessage(message);
-            socket.close();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, message.length(), serverAddr, SERVER_PORT);
+            senderSocket.send(sendPacket);
+            senderSocket.close();
 
         } catch (Exception  e) {
             e.printStackTrace();
